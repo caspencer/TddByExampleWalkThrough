d1d84823b9dbb4f80db245ed47a2d89f41f0e5da
caspencer <cas628@gmail.com>
2012-05-02 15:58:25 -0400

    added solution/project structure, commented out 1st (failing) test

---
+++ TddByExample.Tests/DollarTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace TddByExample.Tests
+{
+    // TODO LIST:
+    // [ ] $5 + 10 CHF = $10 if rate is 2:1
+    // [ ] $5 x 2 = $10
+
+    public class DollarTests
+    {
+        [Fact]
+        public void Multiplication()
+        {
+            //Dollar five = new Dollar(5);
+            //five.times(2);
+            //Assert.Equal(10, five.amount);
+        }
+    }
+}
